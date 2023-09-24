62a880723501e79d6c1d744f1ca47aa23fed4581
taniyamnit <taniya.mnit@gmail.com>
2023-09-24 13:14:48 +0530

    added list , dictionary

---
+++ dictionary.py
@@ -0,0 +1,25 @@
+dict = {"Name":"Taniya","age":31,"subject":["physics","chemistry","maths"], "marks":[100,95,99]}
+print("Creating Dictionary")
+print(dict)
+
+print("Accessing an element using key")
+print("Name of the student is:", dict["Name"])
+
+print("Accessing an element using get")
+print(dict.get("age"))
+print(dict.get("Name"))
+
+print("Creation using Dictionary Comprehension")
+myDict = {x:x**2 for x in [1,2,3,4,5]}
+print(myDict)
+myDict_1 = {x:x+2 for x in [1,2,3,4,5]}
+print(myDict_1)
+
+print("Creation of Dictionary using Zip")
+keys = ["english","maths","physics", "chemistry", "cs"]
+values = [90,92,80,79,95]
+temp = {}
+for key , value in zip(keys,values):
+    temp[key]= value
+print(temp)
+
---
+++ list.py
@@ -0,0 +1,11 @@
+list1 = [1,2,3,"Hello",2.3]
+print(list1)
+
+#creating list containing multiple values of the same type
+list2 = [["Hello", "world"],["this","is","my","list"]]
+print(list2)
+print(list2[0][1])
+print(list2[1][0])
+print(list2[-2][-1])
+print(list1[-1])
+
