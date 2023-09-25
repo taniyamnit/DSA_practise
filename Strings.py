828a9337741abff4d488c72d9f3abd9bf039a82a
taniyamnit <taniya.mnit@gmail.com>
2023-09-25 14:48:01 +0530

    adding strings and frozenset files

---
+++ Strings.py
@@ -0,0 +1,10 @@
+String = "hello"
+print(String)
+
+# Printing First character
+print("\nFirst character of String is: ")
+print(String[0])
+
+# Printing Last character
+print("\nLast character of String is: ")
+print(String[-1])
---
+++ frozenset.py
@@ -0,0 +1,5 @@
+normal_set = (["a","b","c"])
+print("normal_set=", normal_set)
+
+frozen_set = frozenset([1,2,3,4])
+print("frozen_set =", frozen_set)
