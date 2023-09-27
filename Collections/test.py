ac308de89cb4249831de3a293f6f3ebebea0cba0
taniyamnit <taniya.mnit@gmail.com>
2023-09-27 23:32:01 +0530

    trying some programs

---
+++ Collections/test.py
@@ -0,0 +1,16 @@
+x=5
+while(x!=2):
+  print(x)
+  x=x-1
+
+print('cd'.partition('cd'))
+
+print(tuple[1:3] if tuple == ( 'abcd', 786 , 2.23, 'john', 70.2 ) else tuple())
+
+tuple = (1, 2, 3)
+print(2 * tuple)
+
+l = [1,2,3]
+print(2*l)
+
+print(l)
