bfb29fd3d7c74c64ee6442c6d89aa72b95d18526
taniyamnit <taniya.mnit@gmail.com>
2023-09-30 19:36:17 +0530

    adding args & kwargs

---
+++ args.py
@@ -0,0 +1,7 @@
+
+def addition(e,f, *args):
+    add = e+f
+    for num in args:
+        add+= num
+    return add
+print(addition(1,2,3,4,5))
---
+++ kwargs.py
@@ -0,0 +1,5 @@
+
+def addition(**kwargs):
+    for key, value in kwargs.items():
+        print(key , value)
+addition(a= '00', b= '11', c = '22')
